348d43113b097d5fea324be09c50da40fe2b8f77
hiupg <upgod.soares@outlook.com>
2019-04-02 08:30:13 -0400

    aaaaa

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var Test = require('../models/test');
 
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('produtos', {})
+router.get('/:num', function (req, res, next) {
+    res.render('produtos/', {})
 });
 
 
