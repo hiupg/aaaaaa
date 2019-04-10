be0380b625fdd2254e53873b8f50724b1b0a0e60
hiupg <upgod.soares@outlook.com>
2019-04-10 11:20:24 -0400

    atts com gustavo

---
+++ models/categorias.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CategoriaSchema = new Schema({
+    titulo:{ type: String, required:true}
+});
+
+const categoria = mongoose.model("Categorias", CategoriaSchema);
